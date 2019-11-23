612b8cbb19d291ddae9e6085f31b002b3e52e2f7
Sergey Tatsiak <tatsiak@gmail.com>
2019-11-23 15:25:10 +0200

    Add playing sound and blink text when chrome is open and time is not logging. Fetch data on click and on interval

--- background.js
+++ background.js
@@ -1,20 +1,19 @@
 const currentDate = new Date();
-const firstDate = new Date(
-  currentDate.setDate(currentDate.getDate() - currentDate.getDay() + 1)
-);
-const lastDate = new Date(
-  currentDate.setDate(currentDate.getDate() - currentDate.getDay() + 7)
-);
-var firstDateStr = formatDate(firstDate);
-var lastDateStr = formatDate(lastDate);
+const firstDate = new Date(currentDate.setDate(currentDate.getDate() - currentDate.getDay() + 1));
+const lastDate = new Date(currentDate.setDate(currentDate.getDate() - currentDate.getDay() + 7));
+const host = "https://login.timedoctor.com/";
+const sound = new Audio(chrome.runtime.getURL("beep.mp3"));
+const SECOND = 1000;
+const MINUTE = 60 * SECOND;
 
-var currentDateStr = formatDate(new Date());
+let dayOrWeekFlag = false;
+let day = { str: "", hours: 0 };
+let week = { str: "", hours: 0 };
+let status = "";
+let notLoggingInterval = 0;
+let bgColor = "red";
 
-function formatDate(date) {
-  return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
-}
-
-function secondsToHoursMinutes(totalSeconds) {
+const formatSeconds = totalSeconds => {
   totalSeconds = Number(totalSeconds);
   hours = Math.floor(totalSeconds / 3600);
   totalSeconds %= 3600;
@@ -24,54 +23,67 @@ function secondsToHoursMinutes(totalSeconds) {
     hours: Number(hours),
     minutes: Number(minutes)
   };
-}
+};
 
-function setBadge() {
-  let color = "red";
-  if (dayFlag) {
-    chrome.browserAction.setBadgeText({ text: day.str }, () => {});
-    if (day.hours >= 8) {
-      color = "green";
+// prettier-ignore
+const randomColor = () => `#${Math.random().toString(16).substr(-6)}`;
+
+const dateToString = date => `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+
+const setBadge = () => {
+  clearInterval(notLoggingInterval);
+
+  if (status === "Working") {
+    if (dayOrWeekFlag) {
+      chrome.browserAction.setBadgeText({ text: day.str }, () => {});
+      if (day.hours >= 8) {
+        bgColor = "green";
+      } else {
+        bgColor = "red";
+      }
     } else {
-      color = "red";
+      chrome.browserAction.setBadgeText({ text: week.str }, () => {});
+      if (week.hours >= 40 || week.hours >= new Date().getDay() * 8) {
+        bgColor = "green";
+      } else {
+        bgColor = "red";
+      }
     }
   } else {
-    chrome.browserAction.setBadgeText({ text: week.str }, () => {});
-    if (week.hours >= 40 || week.hours >= new Date().getDay() * 8) {
-      color = "green";
-    } else {
-      color = "red";
-    }
+    notLoggingInterval = setInterval(() => {
+      const word = ["log", "time"][Math.round(Math.random())];
+      chrome.browserAction.setBadgeText({ text: word }, () => {});
+      bgColor = randomColor();
+      chrome.browserAction.setBadgeBackgroundColor({ color: bgColor }, () => {});
+      sound.play();
+    }, 2000);
   }
-  chrome.browserAction.setBadgeBackgroundColor({ color }, () => {});
-}
+  chrome.browserAction.setBadgeBackgroundColor({ color: bgColor }, () => {});
+};
 
-const url = `https://login.timedoctor.com/individual-timesheet?fromDate=${firstDateStr}&routeParam=false&selectedUserId=false&timezone=33&toDate=${lastDateStr}`;
-
-let dayFlag = false;
-
-chrome.browserAction.onClicked.addListener(function() {
-  dayFlag = !dayFlag;
-  setBadge();
-});
-
-let day = { str: "", hours: 0 };
-let week = { str: "", hours: 0 };
-
-setInterval(() => {
-  fetch(url)
-    .then(resp => {
-      return resp.json();
+const getData = () => {
+  fetch(`${host}individual-timesheet?fromDate=${dateToString(firstDate)}&routeParam=false&selectedUserId=false&timezone=33&toDate=${dateToString(lastDate)}`)
+    .then(response => {
+      return response.json();
     })
-    .then(res => {
-      const user = res.users[Object.keys(res.users)[0]];
-      week = secondsToHoursMinutes(user.totaltime);
-      day = secondsToHoursMinutes(user.timeline[currentDateStr].worktime);
-
+    .then(json => {
+      const user = json.users[Object.keys(json.users)[0]];
+      week = formatSeconds(user.totaltime);
+      day = formatSeconds(user.timeline[dateToString(new Date())].worktime);
+      status = user.statusCodeInfo;
       setBadge();
     })
     .catch(err => {
       console.log("err: ", err);
-      chrome.tabs.create({ url: "https://login.timedoctor.com/" });
+      chrome.tabs.create({ url: host });
     });
-}, 60 * 1000);
+};
+
+setInterval(() => {
+  getData();
+}, 2 * MINUTE);
+
+chrome.browserAction.onClicked.addListener(() => {
+  dayOrWeekFlag = !dayOrWeekFlag;
+  getData();
+});
